72663ce0b6bf45f30acaff915a150aef84ec6d85
danshapero <shapero.daniel@gmail.com>
2015-02-03 22:24:54 -0800

    Moved most of the mesh outputting stuff to its own file, started getting rid of superfluous includes

---
+++ output_mesh.hpp
@@ -0,0 +1,60 @@
+
+#ifndef OUTPUT_MESH_H
+#define OUTPUT_MESH_H
+
+#include <deal.II/grid/tria.h>
+#include <deal.II/grid/tria_accessor.h>
+#include <deal.II/grid/tria_iterator.h>
+#include <deal.II/grid/tria_boundary_lib.h>
+#include <deal.II/grid/grid_out.h>
+#include <fstream>
+#include <map>
+
+using namespace dealii;
+
+template<int dim>
+void mesh_info(const Triangulation<dim> &tria,
+               const std::string        &filename)
+{
+  std::cout << "Mesh info:" << std::endl
+            << " dimension: " << dim << std::endl
+            << " no. of cells: " << tria.n_active_cells() << std::endl;
+
+  // Next loop over all faces of all cells and find how often each boundary
+  // indicator is used:
+  {
+    std::map<unsigned int, unsigned int> boundary_count;
+    typename Triangulation<dim>::active_cell_iterator
+    cell = tria.begin_active(),
+    endc = tria.end();
+    for (; cell!=endc; ++cell)
+      {
+        for (unsigned int face=0; face<GeometryInfo<dim>::faces_per_cell; ++face)
+          {
+            if (cell->face(face)->at_boundary())
+              boundary_count[cell->face(face)->boundary_indicator()]++;
+          }
+      }
+
+    std::cout << " boundary indicators: ";
+    for (std::map<unsigned int, unsigned int>::iterator it=boundary_count.begin();
+         it!=boundary_count.end();
+         ++it)
+      {
+        std::cout << it->first << "(" << it->second << " times) ";
+      }
+    std::cout << std::endl;
+  }
+
+  // Finally, produce a graphical representation of the mesh to an output
+  // file:
+  std::ofstream out (filename.c_str());
+  GridOut grid_out;
+  grid_out.write_eps (tria, out);
+  std::cout << " written to " << filename
+            << std::endl
+            << std::endl;
+}
+
+
+#endif
