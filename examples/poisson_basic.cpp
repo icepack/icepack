4770c3f99b726107077651264568235785041be4
danshapero <shapero.daniel@gmail.com>
2015-03-09 16:49:20 -0700

    Figured out how to make a deal.II function from a lambda

--- examples/poisson_basic.cpp
+++ examples/poisson_basic.cpp
@@ -26,8 +26,13 @@ public:
 
 int main(int argc, char **argv)
 {
-  // ConstantFunction<2, double> coeff(1.0);
-  const Coefficient<2> coeff;
+  ScalarFunctionFromFunctionObject<2> coeff (
+    [](const Point<2>& p)
+    {
+      return 1.0 + exp(-p[0]*p[0]-p[1]*p[1]);
+    }
+  );
+
   ConstantFunction<2, double> rhs(1.0);
   Triangulation<2> tri = read_gmsh_grid<2>(argv[1]);
 
