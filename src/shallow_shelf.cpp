1f6ec5317360fb554f87c46fd6a42a9f19a644ba
danshapero <shapero.daniel@gmail.com>
2015-03-16 15:12:15 -0700

    Checkpoint, copied over routine to assemble SSA stiffness matrix

--- src/shallow_shelf.cpp
+++ src/shallow_shelf.cpp
@@ -9,13 +9,13 @@ constexpr double nu = 0.5 * pow(A0_cold * exp(-Q_cold / (R * Temp)), -1.0/3)
 
 
 ShallowShelfProblem::ShallowShelfProblem (Triangulation<2>& _triangulation,
-                                          const Function<2>& _surface,
                                           const Function<2>& _bed,
+                                          const Funciton<2>& _thickness,
                                           const Function<2>& _beta)
   :
   triangulation (_triangulation),
-  surface (_surface),
   bed (_bed),
+  thickness (_thickness),
   beta (_beta),
   dof_handler (triangulation),
   fe (FE_Q<2>(1), dim)
@@ -67,6 +67,8 @@ void ShallowShelfProblem::assemble_system ()
   std::vector<types::global_dof_index> local_dof_indices (dofs_per_cell);
 
   std::vector<double> nu_values (n_q_points);
+  std::vector<double> h_values (n_q_points);
+  std::vector<double> beta_values (n_q_points);
   std::vector<Vector<double> > rhs_values (n_q_points,
                                            Vector<double>(2));
 
@@ -78,9 +80,11 @@ void ShallowShelfProblem::assemble_system ()
       fe_values.reinit (cell);
 
       nu_values = nu;  // Check that this actually works...
+      thickness.value_list (fe_values.get_quadrature_points(), h_values);
+      beta.value_list (fe_values.get_quadrature_points(), beta_values);
 
-      DrivingStress.vector_value_list (fe_values.get_quadrature_points(),
-                                       rhs_values);
+      //DrivingStress.vector_value_list (fe_values.get_quadrature_points(),
+      //                                 rhs_values);
 
       for (unsigned int i = 0; i < dofs_per_cell; ++i);
         {
@@ -97,23 +101,80 @@ void ShallowShelfProblem::assemble_system ()
                   cell_matrix(i, j) +=
                     (
                      (fe_values.shape_grad(i, q_point)[component_i] *
-                      fe_values.shape_grad(j, q_point)[component_j] *
-                      nu_values[q_point])
+                      fe_values.shape_grad(j, q_point)[component_j])
                      +
                      (fe_values.shape_grad(i, q_point)[component_j] *
-                      fe_values.shape_grad(j, q_point)[component_i] *
-                      nu_values[q_point])
+                      fe_values.shape_grad(j, q_point)[component_i])
                      +
-                     ((component_i == component_j) ?
-                      (fe_values.shape_grad(i, q_point) *
-                       fe_values.shape_grad(j, q_point) *
-                       nu_values[q_point]) : 0) * 2
-                    )
-                    *
-                    fe_values.JxW(q_point);
+                     2 * ((component_i == component_j) ?
+                          (fe_values.shape_grad(i, q_point) *
+                           fe_values.shape_grad(j, q_point)) : 0)
+                     * nu_values[q_point]
+                     * h_values[q_point]
+                    ) * fe_values.JxW(q_point);
                 } // End of loop over q_point
             } // End of loop over j
         } // End of loop over i
+
+        for (unsigned int i = 0; i < dofs_per_cell; ++i)
+          {
+            const unsigned int
+              component_i = fe.system_to_component_index(i).first;
+
+            for (unsigned int q_point = 0; q_point < n_q_points; ++q_point)
+              {
+                cell_rhs(i) += fe_values.shape_value(i, q_point) *
+                               rhs_values[q_point](component_i) *
+                               fe_values.JxW(q_point);
+              }
+          }
+
+        cell->get_dof_indices (local_dof_indices);
+        for (unsigned int i=0; i<dofs_per_cell; ++i)
+          {
+            for (unsigned int j=0; j<dofs_per_cell; ++j)
+              system_matrix.add (local_dof_indices[i],
+                                 local_dof_indices[j],
+                                 cell_matrix(i,j));
+
+            system_rhs(local_dof_indices[i]) += cell_rhs(i);
+          }
+
     } // End of loop over cell
 
+  hanging_node_constraints.condense (system_matrix);
+  hanging_node_constraints.condense (system_rhs);
+
+  std::map<types::global_dof_index,double> boundary_values;
+  VectorTools::interpolate_boundary_values (dof_handler,
+                                            0,
+                                            ZeroFunction<dim>(dim), // Need right BVs
+                                            boundary_values);
+  MatrixTools::apply_boundary_values (boundary_values,
+                                      system_matrix,
+                                      solution,
+                                      system_rhs);
+
 } // End of AssembleSystem
+
+
+void ShallowShelfProblem::solve ()
+{
+  SolverControl solver_control (1000, 1.0e-12);
+  SolverCG<>    cg (solver_control);
+
+  PreconditionSSOR<> preconditioner;
+  preconditioner.initialize(system_matrix, 1.2);
+
+  cg.solve (system_matrix, solution, system_rhs,
+            preconditioner);
+
+  hanging_node_constraints.distribute (solution);
+}
+
+
+void ShallowShelfProblem::run ()
+{
+  setup_system ();
+  solve ();
+}
