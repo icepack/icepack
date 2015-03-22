75a36c79c7c617241b4a10db8112307c6f1225e9
danshapero <shapero.daniel@gmail.com>
2015-03-22 11:56:28 -0700

    Fixed the mesh builder in SSA example and wrote anonymous functions to generate bed, thickness, beta

--- examples/ssa.cpp
+++ examples/ssa.cpp
@@ -1,10 +1,15 @@
 
 #include "shallow_shelf.hpp"
+#include "ice_surface.hpp"
+#include "driving_stress.hpp"
 
 #include <deal.II/grid/tria_accessor.h>
 #include <deal.II/grid/grid_generator.h>
 
+
 using namespace dealii;
+
+
 
 Triangulation<2> make_domain()
 {
@@ -16,11 +21,8 @@ Triangulation<2> make_domain()
     for (unsigned int i = 0; i < GeometryInfo<2>::vertices_per_cell; ++i)
     {
       Point<2>& v = cell->vertex(i);
-      if (std::abs(v(0) - 1.0) < 1e-5)
-        v(0) = 500.0;
-
-      if (std::abs(v(1) - 1.0) < 1e-5)
-        v(1) = 100.0;
+      Point<2> u = {500 * v(0), 25 * v(0) + 100 * v(1) - 50 * v(0) * v(1)};
+      v = u;
     }
   }
 
@@ -32,7 +34,30 @@ Triangulation<2> make_domain()
 int main(int argc, char **argv)
 {
 
-  Triangulation<2> tri = make_domain();
+  Triangulation<2> tri = make_domain ();
+
+  ScalarFunctionFromFunctionObject<2>
+  bed( [](const Point<2>& x)
+       {
+         return -1000.0;
+       });
+
+  ScalarFunctionFromFunctionObject<2>
+  thickness ([](const Point<2>& x)
+             {
+               return 500.0 - 0.04 * x[1];
+             });
+
+  ScalarFunctionFromFunctionObject<2>
+  beta ([](const Point<2>& x)
+        {
+          return 0.0;
+        });
+
+
+  IceSurface surface (bed, thickness);
+  DrivingStress driving_stress (thickness, surface);
+
 
   return 0;
 }
