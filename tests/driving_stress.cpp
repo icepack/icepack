5a35afc60aee86eb73378a4f42238d464f4d354f
danshapero <shapero.daniel@gmail.com>
2015-03-23 16:12:13 -0700

    Made a separate SurfaceElevation class which explicitly overloads gradient since deal.II doesn't implement that in general

--- tests/driving_stress.cpp
+++ tests/driving_stress.cpp
@@ -5,6 +5,24 @@
 
 using namespace dealii;
 
+
+class SurfaceElevation : public Function<2>
+{
+  double value (const Point<2>& x, const unsigned int component = 0) const
+  {
+    return 200.0 - 0.04 * x(0);
+  }
+
+  Tensor<1, 2> gradient (const Point<2>& x, const unsigned int component = 0) const
+  {
+    Tensor<1, 2> grad;
+    grad[0] = -0.04;
+    grad[1] =  0.0;
+    return grad;
+  }
+};
+
+
 int main (int argc, char **argv)
 {
 
@@ -14,11 +32,7 @@ int main (int argc, char **argv)
            return -400.0 + 0.02 * x(0);
          });
 
-  ScalarFunctionFromFunctionObject<2>
-    surface ([](const Point<2>& x)
-             {
-               return 200.0 - 0.04 * x(0);
-             });
+  SurfaceElevation surface;
 
   double r = rho_water / (rho_water - rho_ice);
   double L = (200 * r - 600) / (0.04 * r - 0.06);
@@ -30,9 +44,13 @@ int main (int argc, char **argv)
   Vector<double> tau_d(2);
 
   driving_stress.vector_value(x, tau_d);
-  if (std::abs(tau_d[0] - 0.04 * thickness.value(x)) > 1e-5
+  if (std::abs(tau_d[0] - rho_ice * gravity * thickness.value(x) * 0.04) > 1e-5
       or std::abs(tau_d[1]) > 1e-5)
   {
+
+    std::cout << tau_d[0] << ", " << tau_d[1] << std::endl;
+    std::cout << rho_ice * gravity * 0.04 * thickness.value(x) << ", " << 0.0 << std::endl;
+
     std::cout << "Wrong value for driving stress "
               << "before grounding line!" << std::endl;
     return 1;
@@ -40,9 +58,12 @@ int main (int argc, char **argv)
 
   x(0) = L + 100.0;
 
-  if (std::abs(tau_d[0] - 0.04 * thickness.value(x)) > 1e-5
+  if (std::abs(tau_d[0] - rho_ice * gravity * thickness.value(x) * 0.04) > 1e-5
       or std::abs(tau_d[1]) > 1e-5)
   {
+    std::cout << tau_d[0] << ", " << tau_d[1] << std::endl;
+    std::cout << rho_ice * gravity * 0.04 * thickness.value(x) << ", " << 0.0 << std::endl;
+
     std::cout << "Wrong value for driving stress "
               << "after grounding line!" << std::endl;
     return 1;
