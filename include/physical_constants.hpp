97d63c8e9e833ae24b7cd405e6224de8aeb79115
danshapero <shapero.daniel@gmail.com>
2015-09-05 14:30:22 -0700

    Started writing MMS test for shallow shelf

--- include/physical_constants.hpp
+++ include/physical_constants.hpp
@@ -5,7 +5,7 @@
 // All units are megapascals / meters / years
 constexpr double year_in_sec = 365.25 * 24 * 3600;
 constexpr double gravity  = 9.81 * year_in_sec * year_in_sec;  //  m / a^2
-constexpr double idealgas = 8.3144621e-3;                      //  kJ / mole * K
+constexpr double ideal_gas = 8.3144621e-3;                      //  kJ / mole * K
 constexpr double rho_ice  = 917 / (year_in_sec * year_in_sec) * 1.0e-6;
 constexpr double rho_water = 1024 / (year_in_sec * year_in_sec) * 1.0e-6;
 
---
+++ tests/shallow_shelf.cpp
@@ -0,0 +1,51 @@
+
+#include <deal.II/grid/grid_generator.h>
+
+#include "shallow_shelf.hpp"
+
+using namespace dealii;
+using namespace ShallowShelfApproximation;
+
+/**
+ * This program tests the correctness of the our implementation of the shallow
+ * shelf approximation using the method of manufactured solutions.
+ * Constant ice flow and constant thickness are a simple solution of SSA.
+ */
+
+
+const double eps = 0.2;
+const double temp = 263.15;
+const double surf = 8 * viscosity(temp, eps) * eps / (rho_ice * gravity);
+
+
+class BoundaryVelocity : public TensorFunction<1, 2>
+{
+public:
+  BoundaryVelocity () : TensorFunction<1, 2>() {}
+  virtual Tensor<1, 2> value (const Point<2>& x) const
+  {
+    Tensor<1, 2> v;
+    v[0] = 100.0 + eps * x[0];
+    v[1] = 0.0;
+    return v;
+  }
+};
+
+
+int main()
+{
+  dealii::deallog.depth_console (0);
+
+  const Point<2> p1(0.0, 0.0), p2(2000.0, 500.0);
+  Triangulation<2> triangulation;
+  GridGenerator::hyper_rectangle(triangulation, p1, p2);
+  triangulation.refine_global(2);
+
+  auto surface = ConstantFunction<2>(surf);
+  auto bed     = ConstantFunction<2>(-2000.0);
+  BoundaryVelocity boundary_velocity;
+
+  ShallowShelf shallow_shelf(triangulation, surface, bed, boundary_velocity);
+
+  return 0;
+}
