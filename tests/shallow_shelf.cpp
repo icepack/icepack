a21d5e5ce984f876622b74926f0c3adcc03d5e59
danshapero <shapero.daniel@gmail.com>
2015-09-18 11:58:34 -0700

    Using exact ice ramp solution from Greve & Blatter for shallow shelf test

--- tests/shallow_shelf.cpp
+++ tests/shallow_shelf.cpp
@@ -11,25 +11,61 @@ using namespace icepack;
 /**
  * This program tests the correctness of the our implementation of the shallow
  * shelf approximation using the method of manufactured solutions.
- * Constant ice flow and constant thickness are a simple solution of SSA.
+ * We use the exact solution for the ice ramp from ch. 6.4 in Greve & Blatter.
  */
 
 
-const double eps = 0.2;
+const double rho = rho_ice * (1 - rho_ice / rho_water);
 const double temp = 263.15;
-const double surf = 8 * viscosity(temp, eps) * eps / (rho_ice * gravity);
+const double A = pow(rho * gravity / 4, 3) * rate_factor(temp);
+
+
+const double u0 = 100;
+const double h0 = 500;
+const double delta_h = 100.0;
+const double length = 2000.0;
+const double width = 500.0;
 
 
 class BoundaryVelocity : public TensorFunction<1, 2>
 {
 public:
   BoundaryVelocity () : TensorFunction<1, 2>() {}
-  virtual Tensor<1, 2> value (const Point<2>& x) const
+
+  Tensor<1, 2> value (const Point<2>& x) const
   {
+    const double q = 1 - pow(1 - delta_h * x[0] / (length * h0), 4);
+
+    // This is the exact solution to the PDE.
     Tensor<1, 2> v;
-    v[0] = 100.0 + eps * x[0];
+    v[0] = u0 + 0.25 * A * q * length * pow(h0, 4) / delta_h;
     v[1] = 0.0;
+
+    // TODO include a fudge factor so the initial guess isn't the exact
+    // solution.
+
     return v;
+  }
+};
+
+
+class SurfaceElevation : public Function<2>
+{
+public:
+  SurfaceElevation() : Function<2>() {}
+
+  double value (const Point<2>& x, const unsigned int) const
+  {
+    return (1 - rho_ice / rho_water) * (h0 - delta_h / length * x[0]);
+  }
+
+  Tensor<1, 2> gradient(const Point<2>&, const unsigned int) const
+  {
+    Tensor<1, 2> ds;
+    ds[0] = -(1 - rho_ice / rho_water) * delta_h / length;
+    ds[1] = 0.0;
+
+    return ds;
   }
 };
 
@@ -38,7 +74,7 @@ int main()
 {
   dealii::deallog.depth_console (0);
 
-  const Point<2> p1(0.0, 0.0), p2(2000.0, 500.0);
+  const Point<2> p1(0.0, 0.0), p2(length, width);
   Triangulation<2> triangulation;
   GridGenerator::hyper_rectangle(triangulation, p1, p2);
 
@@ -47,15 +83,15 @@ int main()
     for (unsigned int face_number = 0;
          face_number < GeometryInfo<2>::faces_per_cell;
          ++face_number)
-      if (cell->face(face_number)->center()(0) > 1999.0)
+      if (cell->face(face_number)->center()(0) > length - 1.0)
         cell->face(face_number)->set_boundary_id (1);
   }
 
   triangulation.refine_global(2);
 
-  auto surface     = ConstantFunction<2>(surf);
   auto bed         = ConstantFunction<2>(-2000.0);
-  auto temperature = ConstantFunction<2>(263.15);
+  auto temperature = ConstantFunction<2>(temp);
+  SurfaceElevation surface;
   BoundaryVelocity boundary_velocity;
 
   ShallowShelf shallow_shelf(triangulation, surface, bed,
@@ -71,9 +107,9 @@ int main()
      VectorFunctionFromTensorFunction<2> (boundary_velocity),
      difference,
      shallow_shelf.quadrature_formula,
-     VectorTools::Linfty_norm);
+     VectorTools::L2_norm);
 
-  const double error = difference.linfty_norm();
+  const double error = difference.l2_norm();
   if (error > 1.0e-10) {
     std::cout << error << std::endl;
     return 1;
