0f37ad089085b55084cfcb662266bed35e9a7645
danshapero <shapero.daniel@gmail.com>
2015-09-19 21:03:51 -0700

    Wrote a script to read .ucd output into python

---
+++ scripts/ucd.py
@@ -0,0 +1,30 @@
+
+import numpy as np
+
+def read(filename):
+    with open(filename, 'r') as f:
+        num_points, num_cells, _, _, _ = map(int, f.readline().split())
+
+        x = np.zeros(num_points, dtype = np.float64)
+        y = np.zeros(num_points, dtype = np.float64)
+
+        for i in range(num_points):
+            _, x[i], y[i], _ = map(float, f.readline().split())
+
+        f.readline()
+
+        cell = np.zeros((num_cells, 4), dtype = int)
+
+        for i in range(num_cells):
+            cell[i, :] = map(lambda k: int(k) - 1, f.readline().split()[3:])
+
+        for i in range(4):
+            f.readline()
+
+        u = np.zeros(num_points, dtype = np.float64)
+        v = np.zeros(num_points, dtype = np.float64)
+
+        for i in range(num_points):
+            _, u[i], v[i] = map(float, f.readline().split())
+
+    return x, y, cell, u, v
