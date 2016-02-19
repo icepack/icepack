41d68ca7b56f441e5f859f477ee510c3cee219ad
danshapero <shapero.daniel@gmail.com>
2016-02-18 18:34:13 -0800

    Templated the linear_solve procedure on Krylov method

--- include/icepack/numerics/linear_solve.hpp
+++ include/icepack/numerics/linear_solve.hpp
@@ -4,26 +4,43 @@
 
 #include <deal.II/lac/sparse_matrix.h>
 #include <deal.II/lac/constraint_matrix.h>
+#include <deal.II/lac/solver_cg.h>
+#include <deal.II/lac/solver_bicgstab.h>
+#include <deal.II/lac/sparse_ilu.h>
 
 namespace icepack {
 
   using dealii::Vector;
   using dealii::SparseMatrix;
   using dealii::ConstraintMatrix;
+  using dealii::SolverControl;
+  using dealii::SolverCG;
+  using dealii::SolverBicgstab;
+  using dealii::SparseILU;
 
   /**
    * Solve a symmetric, positive-definite linear system using sparse ILU and
    * the conjugate gradient method.
    */
+  template <class Solver = SolverCG<> >
   void linear_solve(
     const SparseMatrix<double>& A,
     Vector<double>& u,
     const Vector<double>& f,
     const ConstraintMatrix& constraints
-  );
+  )
+  {
+    SolverControl solver_control(1000, 1.0e-12);
+    // TODO make choice of logging an argument
+    solver_control.log_result(false);
+    Solver solver(solver_control);
 
-  // TODO: procedure for non-symmetric systems, e.g. implicit time
-  // discretization of prognostic equations
+    SparseILU<double> M;
+    M.initialize(A);
+
+    solver.solve(A, u, f, M);
+    constraints.distribute(u);
+  }
 
   // TODO: procedure to solve saddle point problems
 
