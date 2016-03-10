8ffe50ae5aa1788bf9bd9b65a5aa2b6b18c1379a
danshapero <shapero.daniel@gmail.com>
2016-03-10 14:34:09 -0800

    Fixed misleading args in viscosity header + more docs

--- include/icepack/physics/viscosity.hpp
+++ include/icepack/physics/viscosity.hpp
@@ -22,22 +22,41 @@ namespace icepack {
   };
 
 
+  /**
+   * This class is for calculating the rank-4 tensor describing the relation
+   * between the ice strain rate and the membrane stress tensor.
+   */
   struct ConstitutiveTensor
   {
+    /**
+     * Create a constitutive tensor object for a given value of the ice
+     * rheology exponent `n` in Glen's flow law
+     */
     ConstitutiveTensor(const double n);
 
+    /**
+     * Calculate the consitutive tensor including the nonlinear dependence on
+     * the ice strain rate.
+     */
     SymmetricTensor<4, 2> nonlinear(
-      const double temperature,
       const double thickness,
+      const double temperature,
       const SymmetricTensor<2, 2> strain_rate
     ) const;
 
+    /**
+     * Calculate the constitutive tensor for the linearization of the ice flow
+     * equations, including the "effective anisotropy".
+     */
     SymmetricTensor<4, 2> linearized(
-      const double temperature,
       const double thickness,
+      const double temperature,
       const SymmetricTensor<2, 2> strain_rate
     ) const;
 
+    /**
+     * Parameterization for how the ice rheology depends on temperature
+     */
     const Viscosity viscosity;
   };
 
