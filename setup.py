47e059b4b7bfe403764321a2569158e363485ca3
danshapero <shapero.daniel@gmail.com>
2017-12-30 15:41:22 -0800

    Changed directory structure

--- setup.py
+++ setup.py
@@ -5,13 +5,11 @@ from setuptools import setup, find_packages
 
 setup(
     name='icepack',
-    version='0.1.0',
+    version='0.0.3',
     license='GPL v3',
     description='ice sheet flow modelling with the finite element method',
     author='Daniel Shapero',
     url='https://github.com/icepack/icepack',
-    packages=find_packages('src'),
-    package_dir={'': 'src'},
-    py_modules=[splitext(basename(path))[0] for path in glob('src/*.py')]
+    packages=find_packages(exclude=['doc', 'test'])
 )
 
