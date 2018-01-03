f97edeb13e4ffab34d29752c06ce7856806d5081
danshapero <shapero.daniel@gmail.com>
2018-01-02 21:42:19 -0800

    Fixes to interpolation
    The old method for interpolation was failing on vector fields in some
    but not all cases. This patch uses the suggested approach in the
    firedrake documentation of manually setting all the data in the
    interpolated function. It uses a loop though and there should be a way
    to vectorize.

--- icepack/interpolate.py
+++ icepack/interpolate.py
@@ -1,56 +1,49 @@
 
 import numpy as np
+import firedrake
 from firedrake import Expression, Function, interpolate as _interpolate
 
-def _func_wrapper_type(rank, dim):
-    class _FunctionWrapper(Expression):
-        def __init__(self, q, *args, **kwargs):
-            Expression.__init__(self, *args, **kwargs)
-            self._q = q
-
-        def eval(self, value, x):
-            value[:] = np.reshape(self._q(x), dim ** rank)
-
-        def value_shape(self):
-            return tuple((dim for i in range(rank)))
-
-    return _FunctionWrapper
-
-
-def interpolate(q, X):
+def interpolate(f, Q):
     """Interpolate an analytically-defined function to a function space
 
     Parameters
     ----------
-    q
-        Either a `firedrake.Expression` or a callable object, taking in an
-        argument `x` and returning a scalar, vector, or tensor
-    X : firedrake.FunctionSpace
-        The function space that `q` will be interpolated to. If `q` is a
-        scalar/vector/tensor field, `X` must be a function space of the
-        right rank.
+    f : firedrake.Expression, firedrake.Function, or a callable object
+        If callable, must take in values of the dimension of the function
+        space and return either scalar/vector/tensor values
+    Q : firedrake.FunctionSpace
+        The function space to interpolate to
+
+    The function space `Q` must be of the right rank for the type of the
+    range of `f`. For example, if `f` is a vector-valued function, then `Q`
+    must be a vector function space, likewise for tensors or scalars.
 
     Returns
     -------
     firedrake.Function
-        A finite element function defined on `X` with the same nodal values
-        as the function `q`
+        A finite element function defined on `Q` with the same nodal values
+        as the function `f`
     """
-    if isinstance(q, Expression) or isinstance(q, Function):
-        return _interpolate(q, X)
+    if isinstance(f, (firedrake.Expression, firedrake.Function)):
+        return firedrake.interpolate(f, Q)
 
-    if hasattr(q, '__call__'):
-        domain = X.ufl_domain()
-        dim = domain.topological_dimension()
-        if dim != domain.geometric_dimension():
-            raise ValueError('Geometric and topological dimension of function'
-                             'spaces must be equal!')
+    if hasattr(f, '__call__'):
+        domain = Q.ufl_domain()
+        element = Q.ufl_element()
+        if len(element.sub_elements()) > 0:
+            element = element.sub_elements()[0]
 
-        element = X.ufl_element()
-        rank = len(element.value_shape())
+        V = firedrake.VectorFunctionSpace(domain, element)
+        X = firedrake.interpolate(domain.coordinates, V)
 
-        f = _func_wrapper_type(rank, dim)(q)
-        return _interpolate(f, X)
+        q = firedrake.Function(Q)
+
+        # TODO: make this work for vectorized things & get rid of the loop
+        n = q.dat.data.shape[0]
+        for i in range(n):
+            q.dat.data[i] = f(X.dat.data_ro[i, :])
+
+        return q
 
     raise ValueError('Argument must be callable!')
 
