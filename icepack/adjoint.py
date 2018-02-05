12b87c2b81d826fa1756fe51c7dec6f5988450c1
danshapero <shapero.daniel@gmail.com>
2018-02-05 14:30:18 -0800

    Better docstring for adjoint.derivative

--- icepack/adjoint.py
+++ icepack/adjoint.py
@@ -1,18 +1,37 @@
 
 import firedrake
 
-def derivative(J, F, u, p, bc):
+def derivative(J, F, u, p, bc=None):
     """Compute the sensitivity of a functional of the solution of a PDE
     with respect to a parameter
 
-    Given a functional :math:`J(u)` of the solution :math:`u` of the
-    nonlinear equation
+    Let :math:`G(p)` be the solution operator for the nonlinear equation
 
-    ..math::
-       F(u, p) = 0,
+    .. math:: F(u, p) = 0,
 
-    this procedure returns a form representing the derivative of :math:`J`
-    with respect to the parameter :math:`p` using the adjoint method.
+    i.e. :math:`F(G(p), p) = 0` for all :math:`p`. Given a functional
+    :math:`J(u)` of the solution, this function returns the derivative of
+    :math:`J(G(p))` with respect to the parameter :math:`p` using the
+    adjoint method.
+
+    Parameters
+    ----------
+    J : firedrake.Form
+        A functional of the solution of a PDE
+    F : firedrake.Form
+        The weak form of the PDE
+    u : firedrake.Function
+        A solution of the PDE with the given parameters
+    p : firedrake.Function
+        The parameters we're linearizing around
+    bc : firedrake.DirichletBC, optional
+        Boundary conditions for the PDE
+
+    Returns
+    -------
+    dJ : firedrake.Form
+        The derivative of `J` with respect to `p`
+
     """
     dF = firedrake.derivative(F, u)
     dJ = firedrake.derivative(J, u)
