f82d1fdb710a3a1c70cd5dfbff0bf3ebafed6b72
danshapero <shapero.daniel@gmail.com>
2018-10-18 09:38:01 -0700

    Better documentation for inverse module

--- icepack/inverse.py
+++ icepack/inverse.py
@@ -10,6 +10,15 @@
 # The full text of the license can be found in the file LICENSE in the
 # icepack source directory or at <http://www.gnu.org/licenses/>.
 
+"""Solvers for inverse problems
+
+This module contains objects for specifying and solving inverse problems, where
+some unobservable field is estimated based on observations of an observable
+field and a physical model that connects the two. The class `InverseProblem` is
+used to specify the problem to be solved, while the classes that inherit from
+`InverseSolver` are used to solve it.
+"""
+
 import numpy as np
 import scipy.optimize
 import firedrake
@@ -17,8 +26,9 @@ from firedrake import action, adjoint, replace, ln, dx
 
 
 def _bracket(f):
-    """Given a real function of a single variable, return a value `t` such that
-    `f(t) < f(0)`, which can then be used for a more thorough line search"""
+    """Given a decreasing real function of a single variable, return a value
+    `t` such that `f(t) < f(0)`, which can then be used for a more thorough
+    line search"""
     f_0 = f(0)
     t = 1.0
     while True:
@@ -33,14 +43,19 @@ def _bracket(f):
 
 
 class InverseProblem(object):
-    """Stores data for estimating a parameter of a model from observed data
+    """Specifies an inverse problem
 
-    This object stores all of the data needed to estimate a parameter, such
-    as the rheology or friction coefficient of a glacier, from remote
-    sensing measurements. The parameter is estimated through an iterative
-    minimization process; the key variables that need to be stored are the
-    current guess for the parameter, the velocity field computed from that
-    parameter, and the adjoint state field.
+    This object is used to specify an inverse problem, i.e. estimating a
+    parameter :math:`p` from measurements of a field :math:`u`, where the two
+    are linked by some model physics. It consists mainly of a description of
+    the physics model, the model-data misfit and regularization functionals,
+    the initial state and parameters, and any auxiliary data such as extra
+    fields or boundary conditions.
+
+    This object merely specifies what problem to solve, not how to solve it.
+    That functionality is contained in the class `InverseSolver` and the
+    classes that inherit from it, such as `GradientDescentSolver` and
+    `GaussNewtonSolver`.
 
     At present, this class assumes that the model physics are an elliptic
     PDE that arises from an action principle. It is not equipped to deal
@@ -49,8 +64,7 @@ class InverseProblem(object):
 
     def __init__(self, model, method, objective, regularization,
                  state_name, state, parameter_name, parameter,
-                 model_args={}, dirichlet_ids=[],
-                 callback=None):
+                 model_args={}, dirichlet_ids=[]):
         """Initialize the inverse problem
 
         Parameters
@@ -99,7 +113,27 @@ class InverseProblem(object):
 
 
 class InverseSolver(object):
-    """Base class for approximating the solution of an inverse problem"""
+    """Base class for approximating the solution of an inverse problem
+
+    This object stores most of the data needed to iteratively optimize the
+    value of some parameter of a model, such as the rheology or friction of a
+    glacier, to match remote sensing observations. The key variables that need
+    to be stored are the current guess for the parameter, the observable state
+    computed from that parameter, the adjoint state, and the search direction.
+
+    The optimization problem is solved using a line search method; at each step
+    the parameter :math:`p_k` is updated by finding a value :math:`\\alpha_k`
+    such that
+
+    .. math::
+        p_{k + 1} = p_k + \\alpha_k\cdot q_k
+
+    reduces the value of the objective function, where :math:`q_k` is the
+    search direction. This object implements most of the procedures necessary
+    for keeping the parameter, state, and adjoint consistent. Objects that
+    inherit from this one only need to define how the search direction is
+    computed."""
+
     def _setup(self, problem, callback=(lambda s: None)):
         self._problem = problem
         self._callback = callback
@@ -246,7 +280,7 @@ class InverseSolver(object):
 
     def step(self):
         """Perform a line search along the current descent direction to get
-        a new value of the parameters, then compute the new state, adjoint
+        a new value of the parameters, then compute the new state, adjoint,
         and descent direction."""
         p, q = self.parameter, self.search_direction
         t = self.line_search()
@@ -275,26 +309,23 @@ class InverseSolver(object):
 
 
 class GradientDescentSolver(InverseSolver):
-    """Uses line search along the objective function gradient"""
+    """Implementation of `InverseSolver` using the objective function gradient
+    directly for a search direction
+
+    This implementation of inverse solvers uses the search direction
+
+    .. math::
+        q = -M^{-1}dJ
+
+    where :math:`M` is the finite element mass matrix and :math:`dJ` is the
+    gradient of the objective functional. The search direction is easy to
+    compute using this method, but is often poorly scaled, resulting in more
+    expensive bracketing and line search phases."""
     def __init__(self, problem, callback=(lambda s: None)):
-        """Initializes the inverse solver with the right functionals and
-        auxiliary fields
-
-        Parameters
-        ----------
-        problem : InverseProblem
-            The instance of the problem to be solved
-        callback : callable, optional
-            Function to call at the end of every iteration
-        """
         self._setup(problem, callback)
-
-        # Get the solver object into a consistent internal state
         self.update_state()
         self.update_adjoint_state()
         self.update_search_direction()
-
-        # Call the post-iteration function for the first time
         self._callback(self)
 
     def update_search_direction(self):
@@ -309,6 +340,28 @@ class GradientDescentSolver(InverseSolver):
 
 
 class GaussNewtonSolver(InverseSolver):
+    """Implementation of `InverseSolver` using an approximation to the Hessian
+    of the objective functional to approach Newton-like efficiency
+
+    This implementation of inverse solvers uses the search direction
+
+    .. math::
+        q = -H^{-1}dJ
+
+    where :math:`H` is the Gauss-Newton approximation to the Hessian of the
+    objective functional. If :math:`E` is the model-data misfit, :math:`R` is
+    the regularization, and :math:`G` is the linearization of the parameter-to-
+    observation map, then the Gauss-Newton matrix is
+
+    .. math::
+        H = dG^*\\cdot d^2E\\cdot dG + d^2R.
+
+    This matrix consists of only those terms in the Hessian of the full
+    objective functional that are of "first order", i.e. any terms involving
+    :math:`d^2G` are dropped. This search direction is more expensive to solve
+    for than in, say, gradient descent. However, it is almost always properly
+    scaled to the dimensions of the problem and converges in far fewer
+    iterations."""
     def __init__(self, problem, callback=(lambda s: None),
                  search_tolerance=1e-3):
         self._setup(problem, callback)
@@ -322,6 +375,7 @@ class GaussNewtonSolver(InverseSolver):
         self._callback(self)
 
     def gauss_newton_mult(self, q):
+        """Multiply a field by the Gauss-Newton operator"""
         u, p = self.state, self.parameter
 
         dE = firedrake.derivative(self._E, u)
@@ -344,6 +398,13 @@ class GaussNewtonSolver(InverseSolver):
             firedrake.derivative(dR, p, q)
 
     def gauss_newton_energy_norm(self, q):
+        """Compute the energy norm of a field w.r.t. the Gauss-Newton operator
+
+        The energy norm of a field :math:`q` w.r.t. the Gauss-Newton operator
+        :math:`H` can be computed using one fewer linear solve than if we were
+        to calculate the action of :math:`H\\cdot q` on :math:`q`. This saves
+        computation when using the conjugate gradient method to solve for the
+        search direction."""
         u, p = self.state, self.parameter
 
         dE = firedrake.derivative(self._E, u)
@@ -361,6 +422,8 @@ class GaussNewtonSolver(InverseSolver):
                               firedrake.energy_norm(d2R, q))
 
     def update_search_direction(self):
+        """Solve the Gauss-Newton system for the new search direction using the
+        preconditioned conjugate gradient method"""
         p, q, dJ = self.parameter, self.search_direction, self.gradient
 
         dR = firedrake.derivative(self.regularization, self.parameter)
