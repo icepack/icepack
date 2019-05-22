1bbfc7efdfae6f64722045f604e46a276edb056c
danshapero <shapero.daniel@gmail.com>
2019-05-22 16:02:25 -0700

    Making demo notebook kernels default to firedrake

--- demo/sanitize.sh
+++ demo/sanitize.sh
@@ -4,7 +4,8 @@ jq --indent 1 \
     '
     (.cells[] | select(has("outputs")) | .outputs) = []
     | (.cells[] | select(has("execution_count")) | .execution_count) = null
-    | .metadata = {"language_info": {"name":"python", "pygments_lexer": "ipython3"}}
+    | .metadata = {"kernelspec": {"display_name": "firedrake", "language": "python", "name": "firedrake"},
+                   "language_info": {"name":"python", "pygments_lexer": "ipython3"}}
     | .cells[].metadata = {}
     ' $1 | sponge $1
 
