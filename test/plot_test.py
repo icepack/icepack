ff1cf8f6318ecafd6c5e19d007b72c4304f85cba
danshapero <shapero.daniel@gmail.com>
2019-10-17 12:07:42 -0700

    Improve coverage from plotting test

--- test/plot_test.py
+++ test/plot_test.py
@@ -14,14 +14,16 @@ import numpy as np
 import matplotlib.pyplot as plt
 import rasterio
 import firedrake
+from firedrake import interpolate, as_vector
 import icepack, icepack.plot
 
 def test_plot_mesh():
-    N = 32
-    mesh = firedrake.UnitSquareMesh(N, N)
-    fig, ax = plt.subplots()
-    icepack.plot.triplot(mesh, axes=ax)
-    assert ax.legend_ is not None
+    nx, ny = 32, 32
+    Lx, Ly = 1e5, 1e5
+    mesh = firedrake.RectangleMesh(nx, ny, Lx, Ly)
+    fig, axes = icepack.plot.subplots()
+    icepack.plot.triplot(mesh, axes=axes)
+    assert axes.legend_ is not None
 
 
 def test_plot_grid_data():
@@ -55,9 +57,9 @@ def test_plot_grid_data():
 
 def test_plot_field():
     mesh = firedrake.UnitSquareMesh(32, 32)
-    Q = firedrake.FunctionSpace(mesh, 'CG', 1)
+    Q = firedrake.FunctionSpace(mesh, family='CG', degree=1)
     x, y = firedrake.SpatialCoordinate(mesh)
-    u = firedrake.interpolate(x * y, Q)
+    u = interpolate(x * y, Q)
 
     filled_contours = icepack.plot.tricontourf(u)
     assert filled_contours is not None
@@ -72,14 +74,14 @@ def test_plot_field():
 
 
 def test_streamlines():
-    N = 32
-    mesh = firedrake.UnitSquareMesh(N, N)
-    V = firedrake.VectorFunctionSpace(mesh, 'CG', 1)
+    nx, ny = 32, 32
+    mesh = firedrake.UnitSquareMesh(nx, ny)
+    V = firedrake.VectorFunctionSpace(mesh, family='CG', degree=1)
 
     x, y = firedrake.SpatialCoordinate(mesh)
-    v = firedrake.interpolate(firedrake.as_vector((-y, x)), V)
+    v = interpolate(as_vector((-y, x)), V)
 
-    resolution = 1 / N
+    resolution = 1 / np.sqrt(nx * ny)
     radius = 0.5
     x0 = (radius, 0)
     xs = icepack.plot.streamline(v, x0, resolution)
@@ -92,20 +94,35 @@ def test_streamlines():
         assert abs(sum(x**2) - radius**2) < resolution
 
 
+def test_plot_vector_field():
+    nx, ny = 32, 32
+    mesh = firedrake.UnitSquareMesh(nx, ny)
+    V = firedrake.VectorFunctionSpace(mesh, family='CG', degree=1)
+
+    x, y = firedrake.SpatialCoordinate(mesh)
+    u = interpolate(as_vector((x + 0.01, x * y * (1 - y) * (y - 0.5))), V)
+
+    arrows = icepack.plot.quiver(u)
+    assert arrows is not None
+
+    streamlines = icepack.plot.streamplot(u, density=1 / nx, precision=1 / nx)
+    assert streamlines is not None
+
+
 def test_plot_extruded_field():
-    Nx, Ny = 32, 32
-    mesh2d = firedrake.UnitSquareMesh(Nx, Ny)
+    nx, ny = 32, 32
+    mesh2d = firedrake.UnitSquareMesh(nx, ny)
     mesh3d = firedrake.ExtrudedMesh(mesh2d, layers=1)
     x, y, z = firedrake.SpatialCoordinate(mesh3d)
 
     Q = firedrake.FunctionSpace(mesh3d, family='CG', degree=2,
                                 vfamily='GL', vdegree=4)
-    q = firedrake.interpolate((x**2 - y**2) * (1 - z**4), Q)
+    q = interpolate((x**2 - y**2) * (1 - z**4), Q)
     q_contours = icepack.plot.tricontourf(q)
     assert q_contours is not None
 
     V = firedrake.VectorFunctionSpace(mesh3d, dim=2, family='CG', degree=2,
                                       vfamily='GL', vdegree=4)
-    u = firedrake.interpolate(firedrake.as_vector((1 - z**4, 0)), V)
+    u = interpolate(as_vector((1 - z**4, 0)), V)
     u_contours = icepack.plot.tricontourf(u)
     assert u_contours is not None
