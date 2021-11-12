b87cc0b21de230c5c079b602d7c3f8234ad42a8e
hoffmaao <hoffmaao@uw.edu>
2021-11-12 08:05:22 -0800

    minor changes to vertical velocity test

--- test/vertical_velocity_test.py
+++ test/vertical_velocity_test.py
@@ -17,41 +17,39 @@ import icepack.utilities
 from icepack.constants import (ice_density as ρ_I, water_density as ρ_W,
                                gravity as g, glen_flow_law as n)
 
-Lx, Ly = 20e3, 20e3
-nx, ny = 48, 48
-
-mesh2d = firedrake.RectangleMesh(nx, ny, Lx, Ly)
-mesh = firedrake.ExtrudedMesh(mesh2d, layers=1)
-
-Q = firedrake.FunctionSpace(mesh, family='CG', degree=2, vfamily='DG', vdegree=0)
-Q3D = firedrake.FunctionSpace(mesh, family='DG', degree=2, vfamily='GL', vdegree=6)
-V = firedrake.VectorFunctionSpace(mesh, dim=2, family='CG', degree=2, vfamily='GL', vdegree=5)
-# note we should call the families in the vertical velocity solver.
-
-x, y, ζ = firedrake.SpatialCoordinate(mesh)
-
-u_inflow=1.0
-v_inflow=2.0
-mu=.003
-mv=.001
-
-b=firedrake.interpolate(firedrake.Constant(0.0),Q)
-s=firedrake.interpolate(firedrake.Constant(1000.0),Q)
-h = firedrake.interpolate(s-b,Q)
-u=firedrake.interpolate(firedrake.as_vector((mu*x+u_inflow,mv*y+v_inflow)),V)
-
-m=-0.01
-
-def analytic_vertical_velocity(h,ζ,mu,mv,m,Q3D):
-	return firedrake.interpolate(firedrake.Constant(m)-(firedrake.Constant(mu+mv)*h*ζ),Q3D)
-
-w=firedrake.interpolate(icepack.utilities.vertical_velocity(u,h,m=m)*h,Q3D)
-w_analytic=analytic_vertical_velocity(h,ζ,mu,mv,m,Q3D)
 
 
-assert(np.mean(np.abs(w.dat.data-w_analytic.dat.data))<10e-9)
+def test_vertical_velocity():
+
+    Lx, Ly = 20e3, 20e3
+    nx, ny = 48, 48
+
+    mesh2d = firedrake.RectangleMesh(nx, ny, Lx, Ly)
+    mesh = firedrake.ExtrudedMesh(mesh2d, layers=1)
+    Q = firedrake.FunctionSpace(mesh, family='CG', degree=2, vfamily='DG', vdegree=0)
+    Q3D = firedrake.FunctionSpace(mesh, family='DG', degree=2, vfamily='GL', vdegree=6)
+    V = firedrake.VectorFunctionSpace(mesh, dim=2, family='CG', degree=2, vfamily='GL', vdegree=5)
+    # note we should call the families in the vertical velocity solver.
+
+    x, y, ζ = firedrake.SpatialCoordinate(mesh)
+
+    u_inflow=1.0
+    v_inflow=2.0
+    mu=.003
+    mv=.001
+
+    b=firedrake.interpolate(firedrake.Constant(0.0),Q)
+    s=firedrake.interpolate(firedrake.Constant(1000.0),Q)
+    h = firedrake.interpolate(s-b,Q)
+    u=firedrake.interpolate(firedrake.as_vector((mu*x+u_inflow,mv*y+v_inflow)),V)
+
+    m=-0.01
+
+    def analytic_vertical_velocity(h,ζ,mu,mv,m,Q3D):
+	    return firedrake.interpolate(firedrake.Constant(m)-(firedrake.Constant(mu+mv)*h*ζ),Q3D)
+
+    w=firedrake.interpolate(icepack.utilities.vertical_velocity(u,h,m=m)*h,Q3D)
+    w_analytic=analytic_vertical_velocity(h,ζ,mu,mv,m,Q3D)
 
 
-
-
-
+    assert(np.mean(np.abs(w.dat.data-w_analytic.dat.data))<10e-9)
