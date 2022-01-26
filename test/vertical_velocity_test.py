b0c91302b59e23c287538280b6a9dda12d492953
Daniel Shapero <shapero.daniel@gmail.com>
2022-01-25 18:24:17 -0800

    Make vertical velocity computation work in xz geometry
    Add Jess to utils copyright + name changes in vertical velocity

--- test/vertical_velocity_test.py
+++ test/vertical_velocity_test.py
@@ -13,28 +13,17 @@
 import numpy as np
 import firedrake
 import icepack
-import icepack.utilities
-from icepack.constants import (
-    ice_density as ρ_I,
-    water_density as ρ_W,
-    gravity as g,
-    glen_flow_law as n,
-)
 
 
-def test_vertical_velocity():
-
+def test_vertical_velocity_xyz():
     Lx, Ly = 20e3, 20e3
     nx, ny = 48, 48
 
     mesh2d = firedrake.RectangleMesh(nx, ny, Lx, Ly)
     mesh = firedrake.ExtrudedMesh(mesh2d, layers=1)
-    Q = firedrake.FunctionSpace(mesh, family="CG", degree=2, vfamily="DG", vdegree=0)
-    Q3D = firedrake.FunctionSpace(mesh, family="DG", degree=2, vfamily="GL", vdegree=6)
-    V = firedrake.VectorFunctionSpace(
-        mesh, dim=2, family="CG", degree=2, vfamily="GL", vdegree=5
-    )
-    # note we should call the families in the vertical velocity solver.
+    Q = firedrake.FunctionSpace(mesh, "CG", 2, vfamily="DG", vdegree=0)
+    Q3D = firedrake.FunctionSpace(mesh, "DG", 2, vfamily="GL", vdegree=6)
+    V = firedrake.VectorFunctionSpace(mesh, "CG", 2, dim=2, vfamily="GL", vdegree=5)
 
     x, y, ζ = firedrake.SpatialCoordinate(mesh)
 
@@ -57,7 +46,42 @@ def test_vertical_velocity():
             firedrake.Constant(m) - (firedrake.Constant(mu + mv) * h * ζ), Q3D
         )
 
-    w = firedrake.interpolate(icepack.utilities.vertical_velocity(u, h, m=m) * h, Q3D)
+    expr = icepack.vertical_velocity(velocity=u, thickness=h, basal_mass_balance=m)
+    w = firedrake.interpolate(expr * h, Q3D)
     w_analytic = analytic_vertical_velocity(h, ζ, mu, mv, m, Q3D)
+
+    assert np.mean(np.abs(w.dat.data - w_analytic.dat.data)) < 10e-9
+
+
+def test_vertical_velocity_xz():
+    Lx = 20e3
+    nx = 48
+
+    mesh1d = firedrake.IntervalMesh(nx, Lx)
+    mesh = firedrake.ExtrudedMesh(mesh1d, layers=1)
+    Q = firedrake.FunctionSpace(mesh, "CG", 2, vfamily="DG", vdegree=0)
+    Q_xz = firedrake.FunctionSpace(mesh, "DG", 2, vfamily="GL", vdegree=6)
+    V = firedrake.FunctionSpace(mesh, "CG", 2, vfamily="GL", vdegree=5)
+
+    x, ζ = firedrake.SpatialCoordinate(mesh)
+
+    u_inflow = 1.0
+    mu = 0.003
+
+    b = firedrake.interpolate(firedrake.Constant(0.0), Q)
+    s = firedrake.interpolate(firedrake.Constant(1000.0), Q)
+    h = firedrake.interpolate(s - b, Q)
+    u = firedrake.interpolate(u_inflow + mu * x, V)
+
+    m = -0.01
+
+    def analytic_vertical_velocity(h, ζ, mu, m, Q_xz):
+        return firedrake.interpolate(
+            firedrake.Constant(m) - (firedrake.Constant(mu) * h * ζ), Q_xz
+        )
+
+    expr = icepack.vertical_velocity(velocity=u, thickness=h, basal_mass_balance=m)
+    w = firedrake.interpolate(expr * h, Q_xz)
+    w_analytic = analytic_vertical_velocity(h, ζ, mu, m, Q_xz)
 
     assert np.mean(np.abs(w.dat.data - w_analytic.dat.data)) < 10e-9
